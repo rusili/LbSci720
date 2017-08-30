e6ed0ab24b46f925e1e75cb1002bf21a628110a9
RusiLi <Rusili56@gmail.com>
2017-08-30 15:55:07 -0400

    initial commit

---
+++ app/src/main/java/com/rusi/lbsci720/Activity/ActivityMain.java
@@ -0,0 +1,15 @@
+package com.rusi.lbsci720.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rusi.lbsci720.R;
+
+public class ActivityMain extends AppCompatActivity {
+
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
